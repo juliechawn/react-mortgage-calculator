40010f9c77ecc6cfdd7fadd74c01a4228130ffbd
Julie C  Hawn <juliechawn@gmail.com>
2018-02-28 12:27:57 -0800

    fourth commit

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,45 +5,112 @@ export default class App extends React.Component {
     super();
     // getinitialState
     this.state = {
-      inputs: {},
-      output: '',
+      balance: '', rate: '', term: '', output: '0',
     };
-    this.updateInputs = this.updateInputs.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.calculatePayment = this.calculatePayment.bind(this);
+    this.clearForm = this.clearForm.bind(this);
   }
-  updateInputs(event) {
-    event.preventDefault();
-    // update state
-    console.log('inputs updated');
-    const inputs = {
-      balance: this.balance.value,
-      rate: this.rate.value,
-      term: this.term.value,
-    };
-    this.setState({ inputs });
-    console.log(inputs);
+  handleChange(event) {
+    event.preventDefault(event);
+    this.setState({
+     [event.target.name]: event.target.value,
+    })
+  }
+  calculatePayment(event) {
+    event.preventDefault(event);
+      var p = parseFloat(this.state.balance);
+      var r = parseFloat(this.state.rate) / 100 / 12;
+      var n = parseFloat(this.state.term) * 12;
+      var M = p * ((r * Math.pow((1 + r), n)) / (Math.pow((1 + r), n) - 1));
+      this.setState({ output: M.toFixed(2) });
+  }
+  clearForm() {
     this.form.reset();
   }
-  calcuate(balance, rate, term) {
-    const payment = balance[rate(1 + term)];
-    console.log(payment);
-  };
-
-// calculation function for mortgage payment - set state to bind result to "output" - func has 4 parameters (balance, rate, term, period)- HTML add onClick event calls calculate function
   render() {
     return (
-      <form onSubmit={ this.updateInputs } ref={ (input) => { this.form = input; } }className='form-horizontal'>
+      <form className='form-horizontal'>
         <div className='container'>
           <h1>Mortgage Calculator</h1>
-          <h4>Calculate your mortage balance</h4>
-          <input ref={ (input) => { this.balance = input; } } id='number' name='balance' type='number' placeholder='$ Mortgage Balance' />
-          <input ref={ (input) => { this.rate = input; } } id='number' name='rate' type='number' step='0.01' placeholder='% rate' />
-          <select ref={ (input) => { this.term = input; } } name='term' >
-            <option value='15'> 15 years </option>
+          <h4>Calculate your monthly mortage payment</h4>
+        
+        {/* input for balance */}
+        <div className='row'>
+        <div className='input-group mb-3'>
+        <div className='input-group-prepend'>
+        <span className='input-group-text' id='inputGroup-sizing-default'>$</span>
+        </div>
+          <input onChange={ this.handleChange } 
+                 defaultValue={this.state.balance} 
+                 className='form-control'
+                 aria-label='Default' aria-describedby='inputGroup-sizing-default'  
+                 name='balance' 
+                 type='number' 
+                 placeholder='Enter mortgage balance' />
+        </div>
+        </div>
+
+        {/* input for rate */}
+        <div className='row'>
+        <div className='col'>
+        <div className='input-group'> 
+          <input onChange={ this.handleChange } 
+                 defaultValue={this.state.rate} 
+                 className='form-control' 
+                 aria-label='Default' aria-describedby='inputGroup-sizing-default' 
+                 id='number' 
+                 name='rate' 
+                 type='number' 
+                 step='0.01' 
+                 placeholder='Enter monthly interest rate' />   
+          <div className='input-group-append'>
+          <span className='input-group-text'>%</span>
+          </div>
+        </div>
+        </div>
+
+        {/* select for term */}
+        <div className='col'>
+        <div className='input-group mb-3'>
+          <select onChange={ this.handleChange } 
+                  defaultValue={this.state.term} 
+                  className='custom-select'
+                  aria-label='Default' aria-describedby='inputGroup-sizing-default'   
+                  name='term' >
+            <option value=''>Select Term</option>
+            <option value='15' > 15 years </option>
             <option value='30'> 30 years </option>
           </select>
-          <button type='submit' name='submit' > Submit </button>
-          <div ref={ (output) => { this.output = output; } } />
-        </div>
+         </div> 
+         </div>
+
+         {/* submit and calculate button */}
+         <div className='col'>
+          <button onClick={ this.calculatePayment }
+                  className='btn btn-outline-secondary' 
+                  type='submit' name='submit'  > 
+                  Submit 
+          </button>
+          </div>
+          </div>
+
+          {/* output div */}
+          <div className='row'>
+          <h3><div onChange={ this.handleChange } 
+                name='output'>
+                Your monthly payment is ${this.state.output}
+          </div></h3>
+          </div>
+
+          {/* clear form button */}
+          <div className='row'>    
+          <button onClick={ this.clearForm } 
+                  className='btn btn-outline-secondary'>
+                  Clear Form
+          </button>
+          </div>
+          </div>
       </form>
     );
   }
